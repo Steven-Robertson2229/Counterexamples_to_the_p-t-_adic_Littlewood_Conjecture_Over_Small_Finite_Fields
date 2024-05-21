559471e13de8cf49ac34c93bfb1860f38ea89f12
Steven-Robertson2229 <135606352+Steven-Robertson2229@users.noreply.github.com>
2024-05-21 11:12:16 +0100

    Delete Archive directory

--- Archive/TileRefObject.py
+++
@@ -1,8 +0,0 @@
-# Redundant tile support class from an old version of the Tile class
-
-class TileRef:
-    def __init__(self, image_tile):
-        self.image_tile = image_tile
-
-    def __str__(self):
-        return str(self.image_tile)
